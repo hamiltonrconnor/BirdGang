2e030dba8cf4faf10451a4fd89b9b2979a8ee639
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-02-01 23:12:46 +0000

    MiniBossSpawner Added to spawner manager

--- Bird Gang/Assets/Scripts/PeopleSpawner.cs
+++ Bird Gang/Assets/Scripts/PeopleSpawner.cs
@@ -7,15 +7,19 @@ public class PeopleSpawner : MonoBehaviour
     public int NumberOfPeopleTotal;
     public int NumberOfGoodPeople;
     public int NumberOfBadPeople;
+    public int NumberOfMiniBoss;
     public GameObject badPersonPrefab;
     public GameObject goodPersonPrefab;
-  
+    public GameObject miniBossPrefab;
+    public Renderer renderer;
+
     private int NumberGoodPeopleSpawned;
     private int NumberBadPeopleSpawned;
+    private int NumberMiniBossSpawned;
     private Vector3 minPosition;
     private Vector3 maxPosition;
     private Vector3 centerPosition;
-    public Renderer renderer;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -25,31 +29,21 @@ public class PeopleSpawner : MonoBehaviour
         minPosition = renderer.bounds.min;
         maxPosition = renderer.bounds.max;
         centerPosition = renderer.bounds.center;
-        Debug.Log("Min");
-        Debug.Log(minPosition);
-        Debug.Log("Max");
 
-        Debug.Log(maxPosition);
-        Debug.Log("Center");
-        Debug.Log(centerPosition);
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if (NumberGoodPeopleSpawned < NumberOfGoodPeople)
-        {
-            SpawnGoodPerson();
-        }
-        if (NumberBadPeopleSpawned < NumberOfBadPeople)
-        {
-            SpawnBadPerson();
-        }
+        
+        
+        
     }
 
     private void SpawnGoodPerson()
     {
         GameObject newGoodPerson = Instantiate(goodPersonPrefab);
+        newGoodPerson.transform.parent = this.transform;
         newGoodPerson.transform.position = centerPosition + new Vector3(Random.Range(minPosition.x, maxPosition.x), 0, Random.Range(minPosition.z, maxPosition.z));
         NumberGoodPeopleSpawned++;
     } 
@@ -58,18 +52,49 @@ public class PeopleSpawner : MonoBehaviour
     {
         GameObject newBadPerson = Instantiate(badPersonPrefab);
         newBadPerson.transform.parent = this.transform;
-
         newBadPerson.transform.position =  centerPosition + new Vector3(Random.Range(minPosition.x, maxPosition.x), 0, Random.Range(minPosition.z, maxPosition.z));
         NumberBadPeopleSpawned++;
     }
+    private void SpawnMiniBoss()
+    { 
+        GameObject newMiniBoss = Instantiate(miniBossPrefab);
+        newMiniBoss.transform.parent = this.transform;
+        newMiniBoss.transform.position = centerPosition + new Vector3(Random.Range(minPosition.x, maxPosition.x), 0, Random.Range(minPosition.z, maxPosition.z));
+        NumberMiniBossSpawned++;
+        
+    }
 
-    public void SetNumberOfGoodPeople(int numOfPeople)
+    public void fillMaxGoodPeople(int numOfPeople)
     {
         NumberOfGoodPeople = numOfPeople;
+
+        while(NumberGoodPeopleSpawned < NumberOfGoodPeople)
+        {
+            SpawnGoodPerson();
+        }
+
     }
-    public void SetNumberOfBadPeople(int numOfPeople)
+
+    public void fillMaxBadPeople(int numOfPeople)
     {
         NumberOfBadPeople = numOfPeople;
+        while (NumberBadPeopleSpawned < NumberOfBadPeople)
+        {
+            SpawnBadPerson();
+        }
+    }
+
+    public void fillMaxMiniBoss(int numOfPeople)
+    {
+        NumberOfMiniBoss = numOfPeople;
+        while (NumberMiniBossSpawned < NumberOfMiniBoss)
+        {
+            SpawnMiniBoss();
+        }
+    }
+    public int GetNumberOfMiniBoss(int numOfPeople)
+    {
+        return NumberOfMiniBoss;
     }
     public int GetNumberOfGoodPeople(int numOfPeople)
     {
--- Bird Gang/Assets/SpawnManager.cs
+++ Bird Gang/Assets/SpawnManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 [System.Serializable]
 public class SpawnManager : MonoBehaviour
 {
-     PeopleSpawner[] spawners;
+    public int NumberOfMiniBossTotal;
+    private float spawnDelay;
+    private float nextSpawnTime;
+    PeopleSpawner[] spawners;
     // Start is called before the first frame update
     void Start()
     {
         spawners= GetComponentsInChildren<PeopleSpawner>();
-        
+        NumberOfMiniBossTotal = 5; //this can be changed for however mini bosses we want
+        spawnDelay = 5f; //this can be changed from 5 seconds to maybe 120 - so a mini boss appears at the start of every new "wave".
+
+
     }
 
     // Update is called once per frame
@@ -18,8 +24,17 @@ public class SpawnManager : MonoBehaviour
     {
         foreach (var spawner in spawners)
         {
-            spawner.SetNumberOfGoodPeople(15);
-            spawner.SetNumberOfBadPeople(15);
+            spawner.fillMaxGoodPeople(15);
+            spawner.fillMaxBadPeople(15);
+            
+        }
+        
+        if(Time.time >= nextSpawnTime)
+        {
+            int index = Random.Range(0, spawners.Length);
+            spawners[index].fillMaxMiniBoss(NumberOfMiniBossTotal);
+            nextSpawnTime = Time.time + spawnDelay;
+
         }
     }
 }
